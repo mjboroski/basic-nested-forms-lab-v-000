e3a50836aa3c2f8e1c552271179a065e15d8ca73
Michael Boroski <mjboroski@gmail.com>
2018-06-22 21:20:03 +0000

    automatically backed up by learn

--- db/schema.rb
+++ db/schema.rb
@@ -11,14 +11,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160114135651) do
+ActiveRecord::Schema.define(version: 20160113001515) do
 
   create_table "ingredients", force: :cascade do |t|
     t.string   "name"
     t.string   "quantity"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.integer  "recipe_id"
   end
 
   create_table "recipes", force: :cascade do |t|
