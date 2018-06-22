240ad7f73c76bfa6e588a7b9a6bafd9272b2b37b
Michael Boroski <mjboroski@gmail.com>
2018-06-22 21:34:14 +0000

    Done.

--- db/schema.rb
+++ db/schema.rb
@@ -16,8 +16,9 @@ ActiveRecord::Schema.define(version: 20160114135651) do
   create_table "ingredients", force: :cascade do |t|
     t.string   "name"
     t.string   "quantity"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.string   "ingredient_type"
+    t.datetime "created_at",      null: false
+    t.datetime "updated_at",      null: false
     t.integer  "recipe_id"
   end
 
