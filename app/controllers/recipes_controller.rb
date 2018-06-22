55602ab2e55c049350a7eb690b849560e3ad0149
Michael Boroski <mjboroski@gmail.com>
2018-06-22 21:14:29 +0000

    automatically backed up by learn

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -9,8 +9,24 @@ class RecipesController < ApplicationController
 
   def new
     @recipe = Recipe.new
+    @recipe.ingredients.build(ingredient_type: 'first')
+    @recipe.ingredients.build(ingredient_type: 'second')
   end
 
   def create
+    recipe = Recipe.create(recipe_params)
+    redirect_to recipe_path
+  end
+
+private
+  def recipe_params
+    params.require(:recipe).permit(
+      :title,
+      ingredients_attributes: [
+        :name,
+        :quantity,
+        :ingredient_type
+      ]
+    )
   end
 end
